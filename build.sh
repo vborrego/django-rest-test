7e1152d7a8a9edfea7ad9230d5dbea8a7f39c430
vborrego <vitor.mln.borrego@gmail.com>
2021-07-23 20:11:21 +0100

    Updated code to build react app inside docker container.

--- build.sh
+++ build.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
-PATH=$PATH:./node_modules/typescript/bin
-echo Install NPM packages
-npm install
-echo Compile project
-tsc
-echo Create application bundle
-nodejs ./node_modules/webpack/bin/webpack.js --config webpack.config.js
-mkdir -p static/app/dist/
-cp main.html static/app/
-cp dist/bundle.js static/app/dist/
-cp main.css static/app/dist/
 mkdir target
-cp start.sh manage.py static tutorial target/ -R
+cp start.sh manage.py static tutorial  *tsx *ts *js *json *css *html target/ -R
--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 cd /app
+echo Build react app
+npm install 
+./node_modules/typescript/bin/tsc 
+node ./node_modules/webpack/bin/webpack.js --config webpack.config.js
+mkdir -p static/app/dist/
+cp main.html static/app/
+cp dist/bundle.js static/app/dist/
+cp main.css static/app/dist/
+echo Migrate and run django rest app
 python3 manage.py migrate
+echo Configure admin user
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', ' admin@example.com', '12345678')" | python3 manage.py shell
 python3 manage.py runserver 0.0.0.0:8000
